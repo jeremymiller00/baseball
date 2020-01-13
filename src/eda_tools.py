3132105d7fc53ca7d2ab8d108ed5720091992dfd
Jeremy <jeremymiller00@gmail.com>
2020-01-12 17:45:15 -0800

    mongo functions

--- src/eda_tools.py
+++ src/eda_tools.py
@@ -3,6 +3,8 @@ A suite of functions to make eda plots with baseball data
 '''
 import matplotlib.pyplot as plt
 import pandas as pd
+from pymongo import MongoClient
+
 
 def plot_stats_over_time(year_index, df, stats_to_plot, games_per_year):
     '''
@@ -29,3 +31,55 @@ def plot_stats_over_time(year_index, df, stats_to_plot, games_per_year):
     ax.set_title("Yearly Totals Per Game Over Time", fontsize=20)
     ax.set_xlabel("Year")
     plt.show
+
+
+def _connect_mongo(host, port, username, password, db):
+    """ A util for making a connection to mongo """
+
+    if username and password:
+        mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
+        conn = MongoClient(mongo_uri)
+    else:
+        conn = MongoClient(host, port)
+
+    return conn[db]
+
+
+def read_database(db, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
+    """ Read from Mongo and Store into DataFrame """
+
+    # Connect to MongoDB
+    db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
+
+    dfs = []
+
+    # Get list of collections
+    for name in db.list_collection_names():
+        # Make a query to the specific DB and Collection
+        cursor = db[name].find(query)
+        # Expand the cursor and construct the DataFrame
+        df =  pd.DataFrame(list(cursor))
+        # Delete the _id
+        if no_id:
+            del df['_id']
+        dfs.append(df)
+        
+    return pd.concat(dfs)
+
+def read_collection(db, collection, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
+    """ Read from Mongo and Store into DataFrame """
+
+    # Connect to MongoDB
+    db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
+
+    # Make a query to the specific DB and Collection
+    cursor = db[collection].find(query)
+
+    # Expand the cursor and construct the DataFrame
+    df =  pd.DataFrame(list(cursor))
+
+    # Delete the _id
+    if no_id:
+        del df['_id']
+
+    return df
